d1717248d749275cf5456677452513f22906b917
biyasto <biyasto@gmail.com>
2022-11-19 14:05:33 +0700

    Quick solution (runs synchronously)

--- Assets/Samples/Addressables/1.21.1/Addressables Utility/AddressablesUtility.cs
+++ Assets/Samples/Addressables/1.21.1/Addressables Utility/AddressablesUtility.cs
@@ -1,27 +1,28 @@
-﻿using UnityEngine.AddressableAssets;
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 /// <summary>
 /// A utility class for various Addressables functionality
 /// </summary>
-public static class AddressablesUtility
+public static class Addressables_Utility
 {
     /// <summary>
     /// Get the address of a given AssetReference.
     /// </summary>
     /// <param name="reference">The AssetReference you want to find the address of.</param>
     /// <returns>The address of a given AssetReference.</returns>
-    public static string GetAddressFromAssetReference(AssetReference reference)
+    public static async Task<string> GetAddressFromAssetReference(AssetReference reference)
     {
-        /*var loadResourceLocations = Addressables.LoadResourceLocationsAsync(reference);
-        var result = loadResourceLocations.WaitForCompletion();
-        if (result.Count > 0)
-        {
-            string key = result[0].PrimaryKey;
-            Addressables.Release(loadResourceLocations);
-            return key;
-        }
-
-        Addressables.Release(loadResourceLocations);*/
+        var loadResourceLocations = Addressables.LoadResourceLocationsAsync(reference);
+        await loadResourceLocations.Task;
+        Addressables.Release(loadResourceLocations);
+        
+        if (loadResourceLocations.Status != AsyncOperationStatus.Succeeded)
+            throw new Exception("Task failed to complete!");
+        var result = loadResourceLocations.Result;
+        Addressables.Release(loadResourceLocations);
         return string.Empty;
     }
 }
